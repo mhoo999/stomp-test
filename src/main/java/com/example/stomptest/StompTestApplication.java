0e82f4f62c0c2fb658fc774530f46eb952daaf8a
myeonghoon <mhoo999@naver.com>
2025-09-22 13:48:37 +0900

    init

---
+++ src/main/java/com/example/stomptest/StompTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.stomptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompTestApplication.class, args);
+    }
+
+}
